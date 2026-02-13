f7207b0bf52dd9d5d152849b2a51ffea33fb3642
kuunyangna <161255416+kuunyangna@users.noreply.github.com>
2026-02-13 12:12:32 +0000

    Add Jenkins pipeline for microservice deployment
    This script defines a Jenkins pipeline for deploying a microservice, including stages for checkout, build, test, Docker build, Docker push, and deployment with Helm.

---
+++ vars/deployMicroservice.groovy
@@ -0,0 +1,73 @@
+def call(Map config = [:]) {
+
+    pipeline {
+        agent any
+
+        tools {
+            jdk config.jdk ?: 'jdk'
+            maven config.maven ?: 'mvn'
+        }
+
+        environment {
+            IMAGE_NAME = config.imageName
+            NAMESPACE  = config.namespace ?: "default"
+            RELEASE    = config.helmRelease ?: config.imageName
+            BRANCH     = config.branch ?: "main"
+        }
+
+        stages {
+
+            stage('Checkout') {
+                steps {
+                    git branch: BRANCH, url: config.repoUrl
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    sh "mvn clean package -DskipTests=false"
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    sh "mvn test"
+                }
+            }
+
+            stage('Docker Build') {
+                steps {
+                    sh """
+                        docker build -t ${IMAGE_NAME}:${env.BUILD_NUMBER} .
+                    """
+                }
+            }
+
+            stage('Docker Push') {
+                steps {
+                    withCredentials([usernamePassword(
+                        credentialsId: config.dockerCreds,
+                        usernameVariable: 'DOCKER_USER',
+                        passwordVariable: 'DOCKER_PASS'
+                    )]) {
+                        sh """
+                            echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+                            docker push ${IMAGE_NAME}:${env.BUILD_NUMBER}
+                        """
+                    }
+                }
+            }
+
+            stage('Deploy with Helm') {
+                steps {
+                    sh """
+                        helm upgrade --install ${RELEASE} ${config.helmChart} \
+                          --namespace ${NAMESPACE} \
+                          --set image.repository=${IMAGE_NAME} \
+                          --set image.tag=${env.BUILD_NUMBER}
+                    """
+                }
+            }
+        }
+    }
+}
