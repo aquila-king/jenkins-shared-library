6a5682606ac7d6ba8ea5fc83e4ad0c92fe84c7f5
kuunyangna <161255416+kuunyangna@users.noreply.github.com>
2026-02-13 13:14:54 +0000

    Update deployMicroservice.groovy

--- vars/deployMicroservice.groovy
+++ vars/deployMicroservice.groovy
@@ -1,25 +1,27 @@
+// vars/deployMicroservice.groovy
 def call(Map config = [:]) {
 
     pipeline {
         agent any
 
         tools {
-            jdk config.jdk ?: 'jdk'
+            jdk config.jdk ?: 'jdk17'
             maven config.maven ?: 'mvn'
         }
 
+        // Use string interpolation to make environment block valid
         environment {
-            IMAGE_NAME = config.imageName
-            NAMESPACE  = config.namespace ?: "default"
-            RELEASE    = config.helmRelease ?: config.imageName
-            BRANCH     = config.branch ?: "main"
+            IMAGE_NAME = "${config.imageName ?: 'default-image'}"
+            NAMESPACE  = "${config.namespace ?: 'default'}"
+            RELEASE    = "${config.helmRelease ?: config.imageName ?: 'default-release'}"
+            BRANCH     = "${config.branch ?: 'main'}"
         }
 
         stages {
 
             stage('Checkout') {
                 steps {
-                    git branch: BRANCH, url: config.repoUrl
+                    git branch: "${BRANCH}", url: "${config.repoUrl ?: error('repoUrl must be provided')}"
                 }
             }
 
@@ -46,12 +48,12 @@ def call(Map config = [:]) {
             stage('Docker Push') {
                 steps {
                     withCredentials([usernamePassword(
-                        credentialsId: config.docker-Cred,
+                        credentialsId: "${config.dockerCreds ?: 'docker-cred'}",
                         usernameVariable: 'DOCKER_USER',
                         passwordVariable: 'DOCKER_PASS'
                     )]) {
                         sh """
-                            echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+                            echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
                             docker push ${IMAGE_NAME}:${env.BUILD_NUMBER}
                         """
                     }
@@ -61,12 +63,21 @@ def call(Map config = [:]) {
             stage('Deploy with Helm') {
                 steps {
                     sh """
-                        helm upgrade --install ${RELEASE} ${config.helmChart} \
+                        helm upgrade --install ${RELEASE} ${config.helmChart ?: './helm'} \
                           --namespace ${NAMESPACE} \
                           --set image.repository=${IMAGE_NAME} \
                           --set image.tag=${env.BUILD_NUMBER}
                     """
                 }
+            }
+        }
+
+        post {
+            success {
+                echo "Pipeline completed successfully for ${IMAGE_NAME}!"
+            }
+            failure {
+                echo "Pipeline failed for ${IMAGE_NAME}. Check logs!"
             }
         }
     }
